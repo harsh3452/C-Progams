591d79a8a96508d766de89316108ff85e93c1c95
harsh3452 <harshpatil3452@gmail.com>
2023-09-10 17:26:28 +0530

    program to deleted an elment from an array

---
+++ ArrayElementDeletion.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main(){
+    int i,size,pos;
+    printf("Enter the sie of array:");
+    scanf("%d",&size);
+    int array[size];
+    printf("Enter the elements of the array:\n");
+    for(i=0;i<size;i++)
+    {
+        printf("Array[%d]=",i+1);
+        scanf("%d",&array[i]);
+    }
+    printf("Given array is : ");
+    for(i=0;i<size;i++)
+    {
+        printf("%d  ",array[i]);
+    }
+    printf("\nEnter the position from which the element is supposed to be deleted::");
+    scanf("%d",&pos);
+    for(i=pos-1;i<size;i++)
+    {
+        array[i]=array[i+1];
+    }
+    printf("The new array is :");
+     for(i=0;i<size-1;i++)
+    {
+        printf(" %d  ",array[i]);
+       
+    }
+    return 0;
+    
+}
