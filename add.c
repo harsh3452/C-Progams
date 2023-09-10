23977753b15458d1726a62d9f1d7c00e187c0351
harsh3452 <harshpatil3452@gmail.com>
2023-09-10 16:46:43 +0530

    addition program

---
+++ add.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("Enter any two integers:");
+    scanf("%d %d",&a,&b);
+    printf("The addition of the %d and %d is %d.",a,b,a+b);
+    return 0;
+}
