4eed58b0dda441110de1c334cde51612ceedee0b
harsh3452 <harshpatil3452@gmail.com>
2023-09-16 13:42:31 +0530

    Formated Syntax

--- ArrayElementDeletion.c
+++ ArrayElementDeletion.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int i, size, pos;
 
     printf("Enter the size of array: ");
@@ -10,7 +11,8 @@ int main() {
     int array[100]; // You can choose a maximum size you need
 
     printf("Enter the elements of the array:\n");
-    for (i = 0; i < size; i++) {
+    for (i = 0; i < size; i++)
+    {
         printf("Array[%d] = ", i + 1);
         scanf("%d", &array[i]); // Use & to get the address of array elements
     }
@@ -18,19 +20,24 @@ int main() {
     printf("Enter the position from which the element is supposed to be deleted: ");
     scanf("%d", &pos);
 
-    if (pos >= 0 && pos < size) {
-        for (i = pos; i < size - 1; i++) {
+    if (pos >= 0 && pos < size)
+    {
+        for (i = pos; i < size - 1; i++)
+        {
             array[i] = array[i + 1];
         }
 
         size--; // Decrease the size of the array
 
         printf("The new array is:\n");
-        for (i = 0; i < size; i++) {
+        for (i = 0; i < size; i++)
+        {
             printf("Array[%d] is %d\n", i + 1, array[i]);
         }
-    } else {
-        printf("Invalid position entered.\n");//If entered position is more than size
+    }
+    else
+    {
+        printf("Invalid position entered.\n"); // If entered position is more than size
     }
 
     return 0;
--- add.c
+++ add.c
@@ -1,8 +1,9 @@
-#include<stdio.h>
-int main(){
-    int a,b;
+#include <stdio.h>
+int main()
+{
+    int a, b;
     printf("Enter any two integers:");
-    scanf("%d %d",&a,&b);
-    printf("The addition of the %d and %d is %d.",a,b,a+b);
+    scanf("%d %d", &a, &b);
+    printf("The addition of the %d and %d is %d.", a, b, a + b);
     return 0;
 }
