b7fd66e75980a537e54db98f3eeb126e06f2b888
Harsh Patil <139935226+harsh3452@users.noreply.github.com>
2023-09-29 09:05:37 +0530

    Reverse an Array

--- reversearray.c
+++ reversearray.c
@@ -7,14 +7,14 @@ int i ;
 int main (){
     printf("Enter the array of characters:");
     scanf("%s",array);
-    printf(" OG array = %s \n",array);
+    printf("OG array = %s \n",array);
      
-    int size = strlen(array);
+    int size = strlen(array); //getting length of array
     
     for( i = 0; i<size/2; i++){
-        char temp = array[i];
-        array[i] = array[size-1-i];
-        array[size-1-i] = temp;
+        char temp = array[i]; //Assigning first element of array to temp
+        array[i] = array[size-1-i]; // assigning last element of array at pos size-i-1 to first element i=0
+        array[size-1-i] = temp; // assigning temp value to last postition size-i-1
     }
     printf("New/Reversed Array is %s",array);
 
